ad24ad9aadc9b71e7a12790dbff989c96e2a604a
jentsechen <jentsechen1009@gmail.com>
2026-01-13 22:14:02 +0800

    Fix transpose and verify result

--- hls/tile_transpose.cpp
+++ hls/tile_transpose.cpp
@@ -5,38 +5,47 @@
 #define ROW_SIZE 1024
 #define COL_SIZE 64
 
-ap_uint<SAMPLE_BIT_WIDTH> uram_array[ROW_SIZE * COL_SIZE];
+void write_uram(ap_int<SAMPLE_BIT_WIDTH> *mem, int mem_offset,
+                ap_uint<SAMPLE_BIT_WIDTH> uram_array[ROW_SIZE * COL_SIZE]) {
+  for (int row_index = 0; row_index < ROW_SIZE; row_index++) {
+    for (int col_index = 0; col_index < COL_SIZE; col_index++) {
+#pragma HLS PIPELINE II = 1
+      uram_array[row_index * COL_SIZE + col_index] =
+          mem[row_index * 1024 + col_index + mem_offset];
+    }
+  }
+}
 
-void tile_transpose(ap_int<SAMPLE_BIT_WIDTH> *mem, int mem_offset,
+void read_uram(ap_uint<SAMPLE_BIT_WIDTH> uram_array[ROW_SIZE * COL_SIZE],
+               hls::stream<ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0>> &s0,
+               hls::stream<ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0>> &s1) {
+  for (int col_index = 0; col_index < COL_SIZE / 2; col_index++) {
+    for (int row_index = 0; row_index < ROW_SIZE; row_index++) {
+#pragma HLS PIPELINE II = 1
+      ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0> x0, x1;
+      x0.data = uram_array[row_index * COL_SIZE + col_index];
+      x1.data = uram_array[row_index * COL_SIZE + col_index + 32];
+      s0.write(x0);
+      s1.write(x1);
+    }
+  }
+}
+
+void tile_transpose(ap_int<SAMPLE_BIT_WIDTH> *mem,
                     hls::stream<ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0>> &s0,
                     hls::stream<ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0>> &s1) {
-#pragma HLS bind_storage variable = uram_array type = RAM_T2P impl = URAM
-
 #pragma HLS INTERFACE m_axi port = mem offset = slave bundle = gmem
 #pragma HLS INTERFACE axis port = s0
 #pragma HLS INTERFACE axis port = s1
 #pragma HLS INTERFACE s_axilite port = mem bundle = control
-#pragma HLS INTERFACE s_axilite port = mem_offset bundle = control
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
-  for (int row_index = 0; row_index < ROW_SIZE; row_index++) {
-    for (int col_index = 0; col_index < COL_SIZE; col_index++) {
-#pragma HLS PIPELINE II = 1
-      ap_uint<SAMPLE_BIT_WIDTH> data =
-          mem[row_index * 1024 + col_index + mem_offset];
-      uram_array[row_index * COL_SIZE + col_index] = data;
-    }
-  }
+  for (int tile_index = 0; tile_index < 16; tile_index++) {
+    ap_uint<SAMPLE_BIT_WIDTH> local_uram[ROW_SIZE * COL_SIZE];
+#pragma HLS bind_storage variable = local_uram type = RAM_T2P impl = URAM
 
-  for (int col_index = 0; col_index < COL_SIZE / 2; col_index++) {
-    for (int row_index = 0; row_index < ROW_SIZE; row_index++) {
-#pragma HLS PIPELINE II = 1
-      ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0> x0;
-      x0.data = uram_array[row_index * COL_SIZE + col_index];
-      s0.write(x0);
-      ap_axis<SAMPLE_BIT_WIDTH, 0, 0, 0> x1;
-      x1.data = uram_array[row_index * COL_SIZE + col_index + 32];
-      s1.write(x1);
-    }
+#pragma HLS DATAFLOW
+    write_uram(mem, COL_SIZE * tile_index, local_uram);
+    read_uram(local_uram, s0, s1);
   }
 }
--- verify/test_tile_transpose.py
+++ verify/test_tile_transpose.py
@@ -10,46 +10,53 @@ def print_info(data):
     print("Data type: ", data.dtype)
     print("Dimensions: ", data.ndim)
 
-if __name__ == "__main__":
+def gen_transposed_input():
     input = np.load("data_rx_1024_complex_64.npy")[0]
     input_zp = np.concatenate([input, np.zeros(2**20-len(input))])
+    input_zp_tr = input_zp.reshape(1024, 1024).T.flatten()
+    input_duplicate = np.tile(input_zp_tr, (2, 1)) # duplicate by 2 to make it a matrix
+    np.save("data_rx_1_cpx_64_tr.npy", input_duplicate.astype(np.complex64))
 
+def gen_result_with_py_row_proc():
+    sub_fft_size = 2**10
     col_proc_out = np.load("col_proc_out.npy")
-    print_info(col_proc_out)
-    print(len(col_proc_out))
-    row_proc_out = np.load("row_proc_out.npy")
-    print_info(row_proc_out)
-    print(len(row_proc_out))
+    col_proc_out_tr = col_proc_out.reshape(1024, 1024).T.flatten()
+    result = []
+    for i in range(1024):
+        result.append(np.fft.fft(col_proc_out_tr[sub_fft_size*i:sub_fft_size*(i+1)]))
+    result = np.array(result).flatten()
+    return result.reshape(sub_fft_size, sub_fft_size).T.flatten()
 
-    sub_fft_size, fft_size = 2**10, 2**20
+def read_result():
+    sub_fft_size = 2**10
+    row_proc_out = np.load("row_proc_out.npy")
+    return row_proc_out.astype(np.complex64).reshape(sub_fft_size, sub_fft_size).T.flatten()
+
+def gen_golden():
+    input = np.load("data_rx_1024_complex_64.npy")[0]
+    input_zp = np.concatenate([input, np.zeros(2**20-len(input))])
+    return np.fft.fft(input_zp)
+
+def calc_sqnr(result, golden):
+    return 10*np.log10(sum(abs(golden)**2) / sum(abs(result-golden)**2))
+
+if __name__ == "__main__":
+    # gen_transposed_input()
     
-    result_concat, golden_concat, error_concat = [], [], []
-    for iter_idx in range(64):
-        result = row_proc_out[sub_fft_size*iter_idx:sub_fft_size*(iter_idx+1)]
-        col_proc_out_tr = col_proc_out[iter_idx::sub_fft_size]
-        golden = np.fft.fft(col_proc_out_tr)
-        result_concat = np.concatenate([result_concat, result])
-        golden_concat = np.concatenate([golden_concat, golden])
-        error_concat = np.concatenate([error_concat, result-golden])
-    result_concat = np.array(result_concat)
-    golden_concat = np.array(golden_concat)
-    error_concat = np.array(error_concat)
-    figure = make_subplots(rows=3, cols=1)
-    figure.add_trace(go.Scatter(y=result_concat.real, name="result, real part"), row=1, col=1)
-    figure.add_trace(go.Scatter(y=result_concat.imag, name="result, imaginary part"), row=2, col=1)
-    figure.add_trace(go.Scatter(y=golden_concat.real, name="golden, real part"), row=1, col=1)
-    figure.add_trace(go.Scatter(y=golden_concat.imag, name="golden, imaginary part"), row=2, col=1)
-    figure.add_trace(go.Scatter(y=error_concat.real, name="error, real part"), row=3, col=1)
-    figure.add_trace(go.Scatter(y=error_concat.imag, name="error, imaginary part"), row=3, col=1)
+    result_with_py_row_proc = gen_result_with_py_row_proc()
+    result = read_result()
+    golden = gen_golden()
+    print("SQNR = {} dB (row proc. by python)".format(calc_sqnr(result_with_py_row_proc, golden)))
+    print("SQNR = {} dB".format(calc_sqnr(result, golden)))
+    
+    figure = make_subplots(rows=2, cols=1)
+    figure.add_trace(go.Scatter(y=result.real, name="result, real part"), row=1, col=1)
+    figure.add_trace(go.Scatter(y=result.imag, name="result, imaginary part"), row=2, col=1)
+    figure.add_trace(go.Scatter(y=golden.real, name="golden, real part"), row=1, col=1)
+    figure.add_trace(go.Scatter(y=golden.imag, name="golden, imaginary part"), row=2, col=1)
     figure.update_layout(
         font=dict(size=20)
     )
-    pof.iplot(figure)
-    
-    # np.save("data_rx_1_cpx_64_tr.npy", input_zp[::sub_fft_size])
-    # input_tr = np.load("data_rx_1_cpx_64_tr.npy")
-    # # for i in range(20):
-    # #     print(abs(input_zp[i*sub_fft_size]-input_tr[i])**2)
-    # print_info(input_tr)
+    # pof.iplot(figure)
 
     print("DONE")
